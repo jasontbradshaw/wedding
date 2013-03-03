8ea28894b57e70f307121c1449c9ed120d18f82c
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2013-03-02 22:27:13 -0600

    Add carousel pausing, more styling

--- js/main.js
+++ js/main.js
@@ -1,38 +1,56 @@
 define(['jquery', 'scroller'], function ($, Scroller) {
 
   $(function () {
-    var scroller = new Scroller('#carousel .scroller', 880, 1);
+    var $carousel = $('#carousel');
+    var $scroller = $carousel.find('.scroller');
+    var $thumbnails = $carousel.find('.thumbnails');
+
+    var scroller = new Scroller($scroller, 880, 1);
 
     var updatePosition = function () {
       scroller.position($(this).index());
     };
 
-    // inject the image picker items into the carousel
-    for (var i = 0; i < $('#carousel .scroller').children().length; i++) {
-      var $picker = $('<li>');
-      $('#carousel .index-picker').append($picker);
-      if (i === 0) {
-        $picker.addClass('selected');
-      }
+    // fill the image picker with clones of the carousel images
+    $scroller.children().each(function (i) {
+      // clone the current carousel image as a thumbnail
+      var $thumbnail = $(this).clone(false);
+      $thumbnail.click(updatePosition);
+      $thumbnails.append($thumbnail);
+    });
 
-      $picker.click(updatePosition);
-    }
-
+    // select the currently thumbnail every time we change images
     scroller.bind('position', function (i) {
-      // select the current image picker
-      $('#carousel .index-picker').children().removeClass('selected')
+      $thumbnails.children().removeClass('selected')
           .filter(':eq(' + scroller.position() + ')').addClass('selected');
     });
 
-    var scrollerInterval = setInterval(function () {
-      // reset if we were at the end
-      if (scroller.atRightEdge()) {
-        scroller.position(0);
-      } else {
-        // otherwise, go to the next image
-        scroller.right();
+    // change to the next image unless disabled
+    var shouldScroll = true;
+    var nextImage = function () {
+      // only scroll if we should scroll at all
+      if (shouldScroll) {
+        // reset if we were at the end
+        if (scroller.atRightEdge()) {
+          scroller.position(0);
+        } else {
+          // otherwise, go to the next image
+          scroller.right();
+        }
       }
-    }, 10 * 1000);
+    };
+
+    // switched for scrolling
+    var startScrolling = function () { shouldScroll = true; };
+    var stopScrolling = function () { shouldScroll = false; };
+
+    // only scroll when the mouse isn't on the carousel
+    $carousel.bind('mouseenter', stopScrolling);
+    $carousel.bind('mouseleave', startScrolling);
+
+    // kick off the scroller
+    setInterval(nextImage, 10 * 1000);
+    scroller.position(0);
   });
 
 });
